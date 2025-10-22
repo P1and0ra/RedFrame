51c092fa125ad40881a7ce62a6a86bce20e9b958
P1and0ra <didkivskiy.d@gmail.com>
2025-10-23 00:33:30 +0200

    Optimization
    Была добавлена щепотка оптимизации и исправлена проблема с прокруткой страницы в право и лево

---
+++ src/components/MainContent/aling.jsx
@@ -0,0 +1,11 @@
+import { align } from "../../assets";
+import './align.css'
+
+function Aling() {
+    return (
+        <div className="aling">
+            <img src={align} alt="Align Control" className="align-image" />
+        </div>
+    );
+}
+export default Aling;
