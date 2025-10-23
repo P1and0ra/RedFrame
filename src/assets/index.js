ff2cfe5bce75c415bb21e06fbf438e30c158d661
P1and0ra <didkivskiy.d@gmail.com>
2025-10-23 22:55:52 +0200

    Changes in index.js
    Movies arrives from jsx to index file

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,3 +7,51 @@ export {
     LandingBackground,
     align
 }
+
+export const movies = [
+        {
+            id: 1,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABUJm2glvnxG20nHzTINFNoFx1xIlNf2qSiWC9D9B-ZHtsrJE_o-N7ZFur75uk9tEfpCOHlgDEXg5ZdSbaeN_Nj7Y2BAixGuXq5c.jpg?r=047",
+            title: "Movie 1",
+        },
+        {
+            id: 2,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABc3q_o_ua9xGEiWZ5I8yp0k7yBT8VDkKhLl4naT5BlN-PIXDn6fUoJb6QOxALe_KtJE3MLHUxs-hgLfXty3xIWKOR-PVBOjI5UIgMiCoidsCdW7PK0v22DQfATL5E9kYlNc-.jpg?r=461",
+            title: "Movie 2",
+        },
+        {
+            id: 3,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABXYdBAe3X5R0Lhv1g-jeMi3UePskTmxzdJIMOmTgOGNcPOxd6t12a0cC67mLL3YGEuhv-kVZCC8KVWaZxl7F3rJ5BPH2jta5Q6xfSzWnMyhevoNXvzKY4Losqv9_LvD7Cbq8.jpg?r=49b",
+            title: "Movie 3",
+        },
+        {
+            id: 4,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABeoohnxFnnker6egKMvwn6DeDzabFIl9ofD4lkS5kkiTPw-oLlWlg5EpZ55JS5_fxaNaPe3XQikLVASgsLctH9EVpSaAa9-tUfqu6sGD8TTXs0N1KWu9-ZWOWYdhA6HajAgl.jpg?r=f17",
+            title: "Movie 4",
+        },
+        {
+            id: 5,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABRveE9UZdTkrDaKaP7hU3_aDg9nLvJtoNAK4MNaFXnVyB3deqeQFzEYRHTaBrBRdLnkGwiS5ICXsmW3uOU_6xn0YLaoiYJTb5lGesXGFxdwiYtJiim4AYG578JQzCR-by0lM.jpg?r=605",
+            title: "Movie 5",
+        },
+        {
+            id: 6,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABRSSTbcc943pMLmRx-WtiBuDhOrTIyPfTe6HjhVoAMKKqmsEvfmjS3gj7KtQ0vhc4t6GS55aivCkL2bWIDUFsJOKlzj6uakNeQ0.jpg?r=d78",
+            title: "Movie 6",
+        },
+        {
+            id: 7,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABa4goq7wDX0WsPmbHF_uES71pySJXkbJAERlLqork8TtbxJQ4Ml0g2m-zo8hGxvNb3iUSuvUmWJWqYr8x6ZNeBgeIdBSd4IzLJrrVeSZYasNbzovmlxEdy_OUEdQ5IrkXmnj.jpg?r=c90",
+            title: "Movie 7",
+        },
+        {
+            id: 8,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABY5ERkjtgObnNhib_S8P_xD9RvELai8a6JFbQUZzWIYUw-21o08oYM_rgj-kpv4Fd93v3625kpJzWiuQCxr4xbxMkJhMHBZImHWC1540Hf1FN1ED0b1aIYM-crU9Hzv2iWNg.jpg?r=56c",
+            title: "Movie 8",
+        },
+        {
+            id: 9,
+            img: "https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABY5ERkjtgObnNhib_S8P_xD9RvELai8a6JFbQUZzWIYUw-21o08oYM_rgj-kpv4Fd93v3625kpJzWiuQCxr4xbxMkJhMHBZImHWC1540Hf1FN1ED0b1aIYM-crU9Hzv2iWNg.jpg?r=56c",
+            title: "Movie 8",
+        },
+    ];
